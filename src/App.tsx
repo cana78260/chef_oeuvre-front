bead239fe9e89b465f8fdf30679a617e53348087
eric <eric.jalfre@sncf.fr>
2023-01-08 17:25:04 +0100

    premier commit, creation premieres pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+ 
+} from "react-router-dom";
+import Welcome from './pages/Welcome';
+import Subscribe from './pages/Subscribe';
+import AboutsUs from './pages/AboutsUs';
+import Main from './pages/Main';
+import Connexion from './pages/Connexion';
+import AdminUser from './pages/AdminUser';
+import Page404 from './pages/Page404';
+import Messagerie from './pages/Messagerie';
+import Services from './pages/Services';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<Welcome />}/>
+
+        <Route path="/welcome" element={<Welcome />}/>
+        <Route path="/subscribe" element= {<Subscribe />}/>
+        <Route path="/aboutus" element={<AboutsUs />}/>
+        <Route path="/main" element= {<Main />} />
+        <Route path="/connexion" element= {<Connexion />}/>
+        <Route path="/adminUser" element= {<AdminUser />}/>
+        <Route path="/*" element= {<Page404 />}/>
+        <Route path="/messagerie" element={<Messagerie />}/>
+        <Route path="/services" element={<Services />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Messagerie.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Messagerie = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Messagerie;
