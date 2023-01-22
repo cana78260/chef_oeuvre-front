c411961d04c53d26e957c2a70832d6cf78c6e9b3
eric <eric.jalfre@sncf.fr>
2023-01-22 14:09:14 +0100

    import images

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from '../components/Card';
 
 const Services = () => {
     return (
-        <div>
-            
-        </div>
+      <div>
+        <Card /> <Card />
+       
+      </div>
     );
 };
 
